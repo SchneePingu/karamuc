7a75fbff5d61a8a60073003672f9895107dafcb6
SchneePingu <>
2023-02-26 22:54:23 +0100

    make release script rename apk files

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 flutter build apk --split-per-abi
+cd ./build/app/outputs/flutter-apk
+mv app-arm64-v8a-release.apk karamuc.apk
+mv app-armeabi-v7a-release.apk karamuc-armeabi-v7a.apk
+mv app-x86_64-release.apk karamuc-x86_64.apk
